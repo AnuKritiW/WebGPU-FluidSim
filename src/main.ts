3543c7df46c9357d608d47a8a22b62158677a12a
AnuKritiW <anu.kriti.w@gmail.com>
2025-02-22 15:34:33 +0000

    Abstract out Render pass code.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import { initWebGPU } from "./gpu/device";
 import { createRenderPipeline } from './gpu/pipeline';
+import { render } from './renderer';
 
 async function main() {
   const canvas = document.createElement("canvas");
@@ -16,27 +17,7 @@ async function main() {
   const pipeline = createRenderPipeline(device, canvasFormat);
 
   // create render pass to draw the quad
-  function render() {
-    const commandEncoder = device.createCommandEncoder();         // begins recording drawing commands
-    const textureView = context.getCurrentTexture().createView(); // gets the canvas as a render target
-    const passEncoder = commandEncoder.beginRenderPass({          // starts a new rendering step
-      colorAttachments: [{
-        view: textureView,
-        loadOp: "clear",
-        storeOp: "store",
-        clearValue: [0, 0, 0, 1], // Black background
-      }],
-    });
-
-    // draw quad
-    passEncoder.setPipeline(pipeline); // tells WebGPU to use the shader
-    passEncoder.draw(4); // draws 4 verts for the quad
-    passEncoder.end();
-    device.queue.submit([commandEncoder.finish()]); // sends commands to GPU for execution
-  }
-
-  render();
-
+  render(device, context, pipeline);
 }
 
 // Call WebGPU initialization
---
+++ src/renderer.ts
@@ -0,0 +1,18 @@
+export function render(device: GPUDevice, context: GPUCanvasContext, pipeline: GPURenderPipeline) {
+  const commandEncoder = device.createCommandEncoder();         // begins recording drawing commands
+  const textureView = context.getCurrentTexture().createView(); // gets the canvas as a render target
+  const passEncoder = commandEncoder.beginRenderPass({          // starts a new rendering step
+    colorAttachments: [{
+      view: textureView,
+      loadOp: "clear",
+      storeOp: "store",
+      clearValue: [0, 0, 0, 1], // Black background
+    }],
+  });
+
+  // draw quad
+  passEncoder.setPipeline(pipeline); // tells WebGPU to use the shader
+  passEncoder.draw(4); // draws 4 verts for the quad
+  passEncoder.end();
+  device.queue.submit([commandEncoder.finish()]); // sends commands to GPU for execution
+}
