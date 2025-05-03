a853e82f93773b348824cccebd92ecc7f5c10d7e
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2025-05-03 17:20:23 +0100

    Increase mouse velocity + sample step.

--- src/input.ts
+++ src/input.ts
@@ -34,6 +34,32 @@ export class MouseHandler {
         (event.clientY - rect.top) / rect.height,
       ];
 
+      const numSteps = 20; // number of interpolation steps
+      for (let i = 1; i <= numSteps; i++) {
+        const t = i / (numSteps + 1);
+        const interpPos: [number, number] = [
+          lastPos[0] + (newPos[0] - lastPos[0]) * t,
+          lastPos[1] + (newPos[1] - lastPos[1]) * t
+        ];
+
+        const interpVel: [number, number] = [
+          interpPos[0] - lastPos[0],
+          interpPos[1] - lastPos[1]
+        ];
+
+        const boostedVel: [number, number] = [
+          interpVel[0] * 5.0, // same speed factor you already used
+          interpVel[1] * 5.0
+        ];
+
+        const data = new Float32Array([
+          interpPos[0], interpPos[1], // posX, posY
+          boostedVel[0], boostedVel[1] // velX, velY
+        ]);
+
+        device.queue.writeBuffer(mouseBuffer, 0, data);
+      }
+
       this.vel = [newPos[0] - lastPos[0], newPos[1] - lastPos[1]];
       this.pos = newPos;
       lastPos = newPos; // update last position for the next frame
@@ -47,9 +73,15 @@ export class MouseHandler {
   }
 
   updateGPUBuffer(device: GPUDevice, buffer: GPUBuffer) {
+    const speedFactor = 5.0;
+    const boostedVel: [number, number] = [
+      this.vel[0] * speedFactor,
+      this.vel[1] * speedFactor
+    ];
+
     const data = new Float32Array([
       this.pos[0], this.pos[1], // posX, posY
-      this.vel[0], this.vel[1]  // velX, velY
+      boostedVel[0], boostedVel[1]  // velX, velY
     ]);
 
     // write to GPU buffer
